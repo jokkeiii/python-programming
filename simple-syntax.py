a0e91e63ada5aef45712cb73c5b55bec7cf9097a
jokija <joakim.ijas@tuni.fi>
2022-12-02 15:07:18 +0200

    created dir and simple-syntax

---
+++ simple-syntax.py
@@ -0,0 +1,13 @@
+# defining variable, python does not need the variable type
+x = 5
+
+# if statetements dont need parentheses and statements end in a colon ":" 
+if x == 5:
+    # python uses indentation to indicate blocks of code
+    # must use comma or str() to print number variables with string
+    print("x is ",x)
+
+
+print("This is okay")
+# example how not to indent
+    print("This is not okay")

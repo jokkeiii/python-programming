00a51fbf1a43b41b0757343863cc06a6dd471293
Jokke <joakim.ijas@gmail.com>
2022-12-02 19:47:30 +0200

    added function

--- small-projects/daytime.py
+++ small-projects/daytime.py
@@ -1,1 +1,13 @@
-# ask for user to input time in hours  
+# ask for user to input time in hours 
+# output time of day from a function
+
+# function to test time of the day
+def timeOfDay(clock):
+    if clock >= 6 and clock <= 11:
+        return "morning"
+    elif clock >= 12 and clock <= 16:
+        return "day"
+    elif clock >= 17 and clock <= 22:
+        return "evening"
+    else:
+        return "night"

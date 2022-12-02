74da0dcc6868b188f5676b53088c583055fb84c4
Jokke <joakim.ijas@gmail.com>
2022-12-02 19:56:25 +0200

    added int input type and fixed formatting

--- small-projects/daytime.py
+++ small-projects/daytime.py
@@ -4,19 +4,19 @@
 # function to test time of the day
 def timeOfDay(clock):
     if clock >= 6 and clock <= 11:
-        return "morning"
+        return "morning."
     elif clock >= 12 and clock <= 16:
-        return "day"
+        return "day."
     elif clock >= 17 and clock <= 22:
-        return "evening"
+        return "evening."
     else:
-        return "night"
+        return "night."
 
 # ask user input
-clock = input("Enter current time in hours: ")
+clock = int(input("Enter current time in hours: "))
 
 # call function with user input and assign it to variable
 time = timeOfDay(clock)
 
 # print user input and function return from a variable
-print("Current time is ",clock," hours and the time of day is ",time)
+print("Current time is",clock,"hours and the time of day is",time)

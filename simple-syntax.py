4a94ec5121638094fa60ee6aaf178ba6ba685c69
jokija <joakim.ijas@tuni.fi>
2022-12-02 15:24:38 +0200

    variable naming and unpacking

--- simple-syntax.py
+++ simple-syntax.py
@@ -22,3 +22,25 @@ a = 55
 A = "abcd"
 # A isn't going overwrite a
 
+# legal variable names are
+smallcake = 1
+BIGCAKE = 2
+tiny_cake = 3
+HUGE_CAKE = 4
+_also_cake = 5
+stillcake5 = 6
+
+# variable names must start with a letter or the underscore character
+# illegal variable names
+'''
+3cake = 1 # starts with a number
+my-cake = 2 # contains dash
+this cake = 3 # contains space
+'''
+
+# unpacking a collection of data to different variables
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
